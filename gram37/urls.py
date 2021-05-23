9a92cc9af0e2ef4588387dd051281db22522face
Msyoka <daviemumo37@gmail.com>
2021-05-23 23:27:00 +0300

    create app urls

---
+++ gram37/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+#from django.contrib.auth import views as av
+
+urlpatterns = [
+    path("", views.index, name = "home"),
+    path("new_post/", views.add_post, name = "add_post"),
+    path("<int:pk>/", views.post_detail, name = "post_detail"),
+    path("<int:pk>", views.like, name = "likes"),
+    path("<int:id>/delete", views.delete_post, name = "delete_post"),
+]
